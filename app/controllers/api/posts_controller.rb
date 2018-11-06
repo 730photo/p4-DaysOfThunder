58e0cd4adb9ddbb8cf60904673bc473801f9ccc0
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-11-06 14:07:20 -0500

    full crud on Posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,38 @@
 class Api::PostsController < ApplicationController
+
+def index
+    @player = Player.find(params[player_id])
+    @posts = @player.posts
+    render json: @posts 
+end
+
+def show
+    @post = Post.find(params[:id])
+    @player = @post.player
+    render json: @post 
+end
+
+def create
+    @player = Player.find(params[:player_id])
+    @post = @player.posts.create!(post_params)
+    @posts = @player.posts
+    render json: @posts
+end
+
+def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    render json: @posts
+end
+
+def destroy
+    @post = Post.find(params[:id]).destroy
+    render json: 200
+end
+
+
+private
+def post_params
+    params.require(:post).permit(:body)
+end
 end
