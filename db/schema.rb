df9f6f84f38518d717ebb930d511ca8d82a0eed6
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-11-07 08:56:18 -0500

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2018_11_06_153055) do
   enable_extension "plpgsql"
 
   create_table "players", force: :cascade do |t|
-    t.bigint "user_id"
+    t.string "name"
+    t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 2018_11_06_153055) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "player"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "players", "users"
   add_foreign_key "posts", "players"
   add_foreign_key "posts", "users"
 end
