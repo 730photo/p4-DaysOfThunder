a9a6c558833ddfb8f770805ac277de93550035be
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-11-13 13:48:31 -0500

    changed all player photos to black and white

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ Post.destroy_all
 
 user = User.create(name: "Reg", player: "Russell Westbrook")
 player = Player.create(name: "Russell Westbrook", img: "https://i.imgur.com/QdF9klJ.jpg")
-player2 = Player.create(name: "Kevin Durant", img: "https://i.imgur.com/WXKRPoM.jpg")
-player3 = Player.create(name: "James Harden", img: "https://i.imgur.com/oupuuTB.jpg")
+player2 = Player.create(name: "Kevin Durant", img: "https://i.imgur.com/EK21ciU.jpg")
+player3 = Player.create(name: "James Harden", img: "https://i.imgur.com/4wfKbvA.jpg")
 post = Post.create(body: "He is the triple-doble KING", player: player)
